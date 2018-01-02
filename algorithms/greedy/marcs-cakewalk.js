12b9472f6fc2a0006d1002608ac7f7daf1813215
Siddhartha <sidduinevitable@gmail.com>
2018-01-02 12:03:27 +0530

    Fixed the sort issue with arrays

--- algorithms/greedy/marcs-cakewalk.js
+++ algorithms/greedy/marcs-cakewalk.js
@@ -3,7 +3,7 @@
 */
 
 function marcsCakewalk(calorie) {
-  let val = calorie.sort().reverse().reduce((a, b, c) => {
+  let val = calorie.sort((a, b) => a - b).reverse().reduce((a, b, c) => {
     return a + (b * Math.pow(2, c));
   }, 0);
 
