b64b3849a1eb46db41d85a31f15a20c56d64405a
Siddhartha <sidduinevitable@gmail.com>
2018-01-03 14:15:52 +0530

    adding optimised code for flipping bits problem

--- algorithms/bit-manipulation/flipping-bits.js
+++ algorithms/bit-manipulation/flipping-bits.js
@@ -2,6 +2,7 @@
   https://www.hackerrank.com/challenges/flipping-bits/problem
 */
 
+// Solution-1
 function processData(input) {
   input = input.split('\n');
   input.shift();
@@ -10,5 +11,15 @@ function processData(input) {
     let val = ~input[i];
     let result = val < 0 ? 4294967296 + val : val;
     console.log(result);
+  }
+}
+
+
+// Solution-2
+function processData(input) {
+  input = input.split('\n');
+  input = input.map(Number);
+  for (let i = 1; i < input.length; i++) {
+    console.log(~input[i] >>> 0);
   }
 }
