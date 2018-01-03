381d4b7190e14df804b573e81ca1823aead10d79
Siddhartha <sidduinevitable@gmail.com>
2018-01-03 14:01:37 +0530

    formatted the code

--- algorithms/greedy/mark-and-toys.js
+++ algorithms/greedy/mark-and-toys.js
@@ -3,16 +3,16 @@
 */
 
 function maxiumumToys(prices, k) {
-    let temp = 0;
-    let count =0;
-    prices = prices.sort((a, b) => a-b);
-    for(let i=0; i<prices.length; i++) {
-       if(temp + prices[i] <= k) {
-           temp = temp + prices[i];
-           count++;
-       } else {
-           break;
-       }
+  let temp = 0;
+  let count = 0;
+  prices = prices.sort((a, b) => a - b);
+  for (let i = 0; i < prices.length; i++) {
+    if (temp + prices[i] <= k) {
+      temp = temp + prices[i];
+      count++;
+    } else {
+      break;
     }
-    return count;
+  }
+  return count;
 }
