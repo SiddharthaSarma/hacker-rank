1adbcc51981695e9afdc7822cc614c719aae9cb4
siddharthasarma <sidduinevitable@gmail.com>
2018-01-05 05:05:46 +0000

    solution for hackerrank in a string problem

---
+++ algorithms/strings/hackerrank-in-a-string.js
@@ -0,0 +1,7 @@
+/*
+  https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem
+*/
+
+function hackerrankInString(s) {
+  return s.match(/.*h.*a.*c.*k.*e.*r.*r.*a.*n.*k/) ? "YES" : "NO";
+}
