76216e139925f9d6290f295aebdaba38be247c44
Siddhartha <sidduinevitable@gmail.com>
2020-04-24 06:33:13 +0530

    solution for maximum draw problem

---
+++ mathematics/fundamentals/maximum-draw.cpp
@@ -0,0 +1,15 @@
+// Works on pigeonhole principle
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    cin >> n;
+    cout << n + 1 << "\n";
+  }
+  return 0;
+}
